e12358d35ae6c8d9eb4a039ece4da27c1fdd7476
SVK5227D <91531509+SVK5227D@users.noreply.github.com>
2023-05-03 19:00:47 +0530

    Task2 ADD input filed

--- input_field.dart
+++ input_field.dart
@@ -1,82 +1,53 @@
-import 'inputfield_button.dart';
 import 'package:flutter/material.dart';
-// ignore_for_file: must_be_immutable
 
 class InputField extends StatefulWidget {
-  TextEditingController controller;
-  VoidCallback saveInput;
-  VoidCallback closeField;
-  InputField({
-    super.key,
-    required this.controller,
-    required this.saveInput,
-    required this.closeField,
-  });
+  const InputField({super.key});
 
   @override
-  State<InputField> createState() => _InputFieldState();
+  State<InputField> createState() => _InputField();
 }
 
-class _InputFieldState extends State<InputField> {
-  var listSelect = ['Sample1', 'Sample2', 'Sample3', 'Sample4'];
-
-  String? currentValue;
-
+class _InputField extends State<InputField> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(top: 55, left: 50, right: 50),
-            child: TextField(
-              controller: widget.controller,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('InputField'), centerTitle: true),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 45, top: 30, right: 45),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  labelText: 'Input1',
+                  labelStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  hintStyle: TextStyle(fontSize: 16),
+                  hintText: '',
                 ),
-                labelText: 'Task',
-                labelStyle:
-                    TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                hintStyle: TextStyle(fontWeight: FontWeight.w900, fontSize: 16),
-                hintText: 'Enter task to add',
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 80, right: 80),
-            child: DropdownButton(
-              hint: const Text(
-                'Select Your Preference',
-                style: TextStyle(fontWeight: FontWeight.bold),
+            Container(
+              padding: const EdgeInsets.only(left: 45, top: 10, right: 45),
+              child: const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  labelText: 'Input2',
+                  labelStyle:
+                      TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  hintStyle: TextStyle(fontSize: 16),
+                  hintText: '',
+                ),
               ),
-              icon: const Icon(Icons.keyboard_arrow_down),
-              items: listSelect.map((String element) {
-                // print(element);
-                return DropdownMenuItem(
-                  value: element,
-                  child: Text(element),
-                );
-              }).toList(),
-              onChanged: (String? userSelctValue) {
-                setState(() {
-                  currentValue = userSelctValue!;
-                });
-              },
-              value: currentValue,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              MyButton(text: "Save", onPressed: widget.saveInput),
-              const SizedBox(width: 8),
-              MyButton(text: "Cancel", onPressed: widget.closeField),
-              const SizedBox(width: 50),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- main.dart
+++ main.dart
@@ -1,13 +1,7 @@
-import 'home_page.dart';
+import 'input_field.dart';
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
-  // Hive
-  await Hive.initFlutter();
-
-  var box = await Hive.openBox('taskBox');
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -16,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Viewdata(),
+      home: InputField(),
     );
   }
 }
